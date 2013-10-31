cdd4e5a41e8913de6573454b89a4679fb2045a8e
phsh <phhedman@gmail.com>
2013-10-31 20:37:44 +0100

    added logging to the Main.class

--- helloworld/src/main/java/se/phh/Main.java
+++ helloworld/src/main/java/se/phh/Main.java
@@ -1,15 +1,18 @@
 package se.phh;
 
+import java.util.logging.Logger;
+
 import se.phh.sayers.Sayer;
 import se.phh.sayers.SayerJUL;
 import se.phh.sayers.SayerSystemErr;
 import se.phh.sayers.SayerSystemOut;
 
 public class Main {
-
+	private static Logger logger = Logger.getLogger(Main.class.getName());
     private static final String MESSAGE = "Hello world!";
 
 	public static void main(String[] args) {
+		logger.info("start of main");
         Message message = new Message(MESSAGE);
         LoudMessage loudMessage = new LoudMessage(MESSAGE);
         QuiteMessage quiteMessage = new QuiteMessage(MESSAGE);
@@ -23,6 +26,7 @@ public class Main {
         sayer.say(quiteMessage);
         sayer = new SayerJUL();
         sayer.say(quiteMessage);
+		logger.info("end of main");
     }
     
 }
