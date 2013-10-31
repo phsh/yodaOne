1104ecda8839afb1d3f8f89e616c278c02324ecd
phsh <phhedman@gmail.com>
2013-10-31 10:55:52 +0100

    fixed bug in editor

--- helloworld/src/main/java/se/phh/Main.java
+++ helloworld/src/main/java/se/phh/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Say hello = new HelloWorld("Hello world!");
-        System.out(hello.say());
+        System.out.println(hello.say());
     }
     
 }
