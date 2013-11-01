4714f5d1f82aba3b6e878024d7fd8a670fde4cf5
phsh <phhedman@gmail.com>
2013-11-01 20:46:46 +0100

    used the WhatToSay

--- helloworld/src/main/java/se/phh/Main.java
+++ helloworld/src/main/java/se/phh/Main.java
@@ -3,8 +3,6 @@ package se.phh;
 import java.util.logging.Logger;
 
 import se.phh.message.Message;
-import se.phh.message.MessageFactory;
-import se.phh.sayers.Sayer;
 import se.phh.sayers.SayerFactory;
 
 public class Main {
@@ -12,18 +10,10 @@ public class Main {
 	public static void main(String[] args) {
 		logger.info("start of main");
 		for(Message m:WhatToSayGenerator.getInstance().getWhatToSay()){
-			Sayer sayer = SayerFactory.getInstance().getSayer(SayerFactory.SAYER_OUT);
-	        sayer.say(m);
-		}
-		for(Message m:WhatToSayGenerator.getInstance().getWhatToSay()){
-			Sayer sayer = SayerFactory.getInstance().getSayer(SayerFactory.SAYER_ERR);
-	        sayer.say(m);
-		}
-		for(Message m:WhatToSayGenerator.getInstance().getWhatToSay()){
-			Sayer sayer = SayerFactory.getInstance().getSayer(SayerFactory.SAYER_JUL);
-	        sayer.say(m);
-		}
-        logger.info("end of main");
+			SayerFactory.getInstance().getSayer(SayerFactory.SAYER_OUT).say(m);
+			SayerFactory.getInstance().getSayer(SayerFactory.SAYER_ERR).say(m);
+			SayerFactory.getInstance().getSayer(SayerFactory.SAYER_JUL).say(m);
+		}logger.info("end of main");
     }
     
 }
