dadd63012979197e43917a9c005d19e89b72cb91
phsh <phhedman@gmail.com>
2013-11-01 21:09:10 +0100

    made main jdk6

--- helloworld/src/main/java/se/phh/Main.java
+++ helloworld/src/main/java/se/phh/Main.java
@@ -4,11 +4,10 @@ import java.util.logging.Logger;
 
 import se.phh.message.Message;
 import se.phh.sayers.Sayer;
-import se.phh.sayers.SayerFactory;
 
 public class Main {
 	private static Logger logger = Logger.getLogger(Main.class.getName());
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		logger.info("start of main");
 		for(Message m:WhatToSayGenerator.getInstance().getWhatToSay()){
 			for(Sayer s: HowToSayItGenerator.getInstance().getSayers()){
