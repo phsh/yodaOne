caf507cafea98f335b74b4b3ee953edce7a40270
phsh <phhedman@gmail.com>
2013-11-02 01:31:46 +0100

    Extraced method

--- helloworld/src/main/java/se/phh/Main.java
+++ helloworld/src/main/java/se/phh/Main.java
@@ -10,11 +10,14 @@ public class Main {
 	public static void main(String... args) {
 		logger.info("start of main");
 		for(MessageContent mc:WhatToSayGenerator.getInstance().getWhatToSay()){
-			for(Sayer s: HowToSayItGenerator.getInstance().getSayers()){
-				s.say(mc);
-			} 
+			 sayWhat(mc);
 		}
 		logger.info("end of main");
     }
-    
+	
+    private static void sayWhat(MessageContent mc){
+    	for(Sayer s: HowToSayItGenerator.getInstance().getSayers()){
+			s.say(mc);
+		}
+    }
 }
