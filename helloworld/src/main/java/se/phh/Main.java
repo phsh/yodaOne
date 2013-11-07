a2b1716f6e8c618c763a25e2ffbb7a9a1631c15b
phsh <phhedman@gmail.com>
2013-11-07 13:15:48 +0100

    removed the logging from Main.class

--- helloworld/src/main/java/se/phh/Main.java
+++ helloworld/src/main/java/se/phh/Main.java
@@ -1,7 +1,5 @@
 package se.phh;
 
-import java.util.logging.Logger;
-
 import se.phh.message.MessageContent;
 import se.phh.sayers.Sayer;
 import se.phh.util.Timer;
@@ -9,22 +7,17 @@ import se.phh.generators.MessageContentGenerator;
 import se.phh.generators.SayerGenerator;
 
 public class Main {
-	private static Logger logger = Logger.getLogger(Main.class.getName());
-	
 	private static MessageContentGenerator messageContentGenerator = MessageContentGenerator.getInstance();
 	private static SayerGenerator sayerGenerator = SayerGenerator.getInstance();
 	
 	public static void main(String... args) {
 		Timer timer = new Timer(Main.class.getName());
 		timer.startTimer();
-		logger.info("start of main");
 		for (MessageContent mc : 
 				messageContentGenerator.get()) {
 			sayWhat(mc);
 		}
-		logger.info("end of main");
 		timer.stopTime();
-		logger.info(timer.toString());
 	}
 
 	private static void sayWhat(MessageContent mc) {
