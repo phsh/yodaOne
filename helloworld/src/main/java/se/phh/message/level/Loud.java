0d6965ee35837a9c692f471801027c1a7d80e097
phsh <phhedman@gmail.com>
2013-11-07 23:06:29 +0100

    added prototype to creating a own logging-levels

---
+++ helloworld/src/main/java/se/phh/message/level/Loud.java
@@ -0,0 +1,13 @@
+package se.phh.message.level;
+
+import java.util.logging.Level;
+
+public class Loud extends Level {
+
+	private static final long serialVersionUID = 1L;
+
+	protected Loud(String arg0, int arg1) {
+		super(arg0, arg1);
+	}
+
+}
---
+++ helloworld/src/main/java/se/phh/message/level/Quite.java
@@ -0,0 +1,13 @@
+package se.phh.message.level;
+
+import java.util.logging.Level;
+
+public class Quite extends Level {
+
+	private static final long serialVersionUID = 1L;
+
+	protected Quite(String name, int value) {
+		super(name, value);
+	}
+
+}
