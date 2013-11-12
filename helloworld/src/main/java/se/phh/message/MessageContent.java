1e7f6bbfb4d6a8187a88d2d1f88dcb626c4851c7
phsh <phhedman@gmail.com>
2013-11-12 16:17:19 +0100

    removed the default constant

--- helloworld/src/main/java/se/phh/message/MessageContent.java
+++ helloworld/src/main/java/se/phh/message/MessageContent.java
@@ -2,9 +2,6 @@ package se.phh.message;
 
 public interface MessageContent {
 
-	public static final StringBuilder DEFAULT = new StringBuilder(
-			"Hello world!");
-
 	StringBuilder getMessage();
 
 	void setMessageValue(StringBuilder messageValue);
