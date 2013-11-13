d8a2bc07c5098bec131e4b25a7bb79196d007e23
phsh <phhedman@gmail.com>
2013-11-13 12:01:28 +0100

    added the MESSAGE_TYPE_LOUD test

--- helloworld/src/test/java/se/phh/factories/TestMessageContentFactory.java
+++ helloworld/src/test/java/se/phh/factories/TestMessageContentFactory.java
@@ -17,9 +17,16 @@ public class TestMessageContentFactory {
 	}
 	
 	@Test
-	public void testGetMessageType(){
+	public void testGetMessageType_MESSAGE_TYPE_DEFAULT(){
 		MessageContentFactory factory = MessageContentFactory.getInstance();
 		MessageContent content = factory.getMessageType(MessageContentFactory.MESSAGE_TYPE_DEFAULT, new StringBuilder("messageValue"));
 		assertEquals("This should be equals", content.getMessage().toString(),"[1] messageValue");
+	}
+	
+	@Test
+	public void testGetMessageType_MESSAGE_TYPE_LOUD(){
+		MessageContentFactory factory = MessageContentFactory.getInstance();
+		MessageContent content = factory.getMessageType(MessageContentFactory.MESSAGE_TYPE_LOUD, new StringBuilder("messageValue"));
+		assertEquals("This should be equals", content.getMessage().toString(),"[LOUD] messageValue");
 	}
 }
