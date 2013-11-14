a9ca14688ec904ae964ec6dfc2d2be1c5f2d19c9
phsh <phhedman@gmail.com>
2013-11-14 09:42:22 +0100

    added a Contructor test for MessageContentList

--- helloworld/src/test/java/se/phh/lists/TestMessageContentList.java
+++ helloworld/src/test/java/se/phh/lists/TestMessageContentList.java
@@ -1,5 +1,15 @@
 package se.phh.lists;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
 
 public class TestMessageContentList {
+
+	@Test
+	public void testConstructor(){
+		MessageContentList list = new MessageContentList();
+		assertNotNull("You should be able to create lists",list);
+	}
+	
 
 }
