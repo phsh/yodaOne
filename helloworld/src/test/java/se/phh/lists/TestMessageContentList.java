c47e496efc57910c1dda85c9cb04c1ea7c682dad
phsh <phhedman@gmail.com>
2013-11-14 17:50:08 +0100

    added a size check of Contructor test

--- helloworld/src/test/java/se/phh/lists/TestMessageContentList.java
+++ helloworld/src/test/java/se/phh/lists/TestMessageContentList.java
@@ -1,4 +1,5 @@
 package se.phh.lists;
+
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
@@ -10,19 +11,21 @@ import se.phh.message.MessageContent;
 public class TestMessageContentList {
 
 	@Test
-	public void testConstructor(){
+	public void testConstructor() {
 		MessageContentList list = new MessageContentList();
-		assertNotNull("You should be able to create lists",list);
+		assertNotNull("You should be able to create lists", list);
+		assertEquals("There should be zero elements", 0, list.get().size());
 	}
-	
+
 	@Test
-	public void testAddMessageContentAndRetrieveIt(){
+	public void testAddMessageContentAndRetrieveIt() {
 		MessageContentList messageContentList = new MessageContentList();
 		Loud setMessage = new Loud();
 		messageContentList.add(setMessage);
 		MessageContent getMessage = messageContentList.get().get(0);
-		assertEquals("The same message",setMessage,getMessage);
-		assertEquals("There ought to be only one",1,messageContentList.get().size());
+		assertEquals("The same message", setMessage, getMessage);
+		assertEquals("There ought to be only one", 1, messageContentList.get()
+				.size());
 	}
-	
+
 }
