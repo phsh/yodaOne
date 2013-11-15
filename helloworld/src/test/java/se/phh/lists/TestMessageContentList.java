b09cdcba9f46b36490f655edac87035ae007094f
phsh <phhedman@gmail.com>
2013-11-15 08:42:14 +0100

    fixed the formating

--- helloworld/src/test/java/se/phh/lists/TestMessageContentList.java
+++ helloworld/src/test/java/se/phh/lists/TestMessageContentList.java
@@ -29,32 +29,33 @@ public class TestMessageContentList {
 		assertEquals("There ought to be only one", 1, messageContentList.get()
 				.size());
 	}
-	
+
 	@Test
 	public void testAdd3MessageContentsAndRetrieve() {
 		MessageContentList messageContentList = new MessageContentList();
-		
+
 		Loud setMessage1 = new Loud();
 		Quiet setMessage2 = new Quiet();
 		Standard setMessage3 = new Standard();
-		
+
 		messageContentList.add(setMessage1);
 		messageContentList.add(setMessage2);
 		messageContentList.add(setMessage3);
-		
+
 		MessageContent getMessage1 = messageContentList.get().get(0);
 		MessageContent getMessage2 = messageContentList.get().get(1);
 		MessageContent getMessage3 = messageContentList.get().get(2);
-		
+
 		assertEquals("The same message, 1", setMessage1, getMessage1);
 		assertEquals("The same message, 2", setMessage2, getMessage2);
 		assertEquals("The same message, 3", setMessage3, getMessage3);
-		
-		assertEquals("There ought to be only three", 3, messageContentList.get()
-				.size());
-		
+
+		assertEquals("There ought to be only three", 3, messageContentList
+				.get().size());
+
 		MessageContent getMessage2_again = messageContentList.get().get(1);
-		assertEquals("The same message, again nr. 2", setMessage2, getMessage2_again);
+		assertEquals("The same message, again nr. 2", setMessage2,
+				getMessage2_again);
 	}
 
 }
