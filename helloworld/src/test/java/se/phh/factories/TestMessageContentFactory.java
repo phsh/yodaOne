ec52d223450e7f0b90e68ef74d8534fdae48affb
phsh <phhedman@gmail.com>
2013-11-16 09:21:37 +0100

    added multi test of the factory

--- helloworld/src/test/java/se/phh/factories/TestMessageContentFactory.java
+++ helloworld/src/test/java/se/phh/factories/TestMessageContentFactory.java
@@ -1,6 +1,7 @@
 package se.phh.factories;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
@@ -58,5 +59,30 @@ public class TestMessageContentFactory {
 		assertEquals("This should be equals", content.getMessage().toString(),
 				"messageValue");
 	}
-	
+
+	@Test
+	public void testGetMessageType_MULIPLE() {
+		MessageContentFactory factory = MessageContentFactory.getInstance();
+		
+		MessageContent content1 = factory.getMessageType(
+				MessageContentFactory.MESSAGE_TYPE_STANDARD, new StringBuilder(
+						"messageValue"));
+		assertNotNull(content1);
+
+		MessageContent content2 = factory.getMessageType(
+				MessageContentFactory.MESSAGE_TYPE_QUIET, new StringBuilder(
+						"messageValue"));
+		assertNotNull(content2);
+
+		MessageContent content3 = factory.getMessageType(
+				MessageContentFactory.MESSAGE_TYPE_LOUD, new StringBuilder(
+						"messageValue"));
+		assertNotNull(content3);
+		
+		MessageContent content4 = factory.getMessageType(947295,
+				new StringBuilder("messageValue"));
+		assertNotNull(content4);
+
+	}
+
 }
